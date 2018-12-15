3f053134e931b6cb2d507e5f2739ea2022b5584d
shubhindia <shubhindia123@gmail.com>
2018-12-15 23:26:43 +0530

    Beautify code :P
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- Regression.py
+++ Regression.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 Copyright @Shubhcyanogen
+
 """
 
 import numpy as np
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import train_test_split
+
 def estimate_coef(x, y):
 
     n = np.size(x)
@@ -19,6 +20,7 @@ def estimate_coef(x, y):
     b_1 = SS_xy / SS_xx
     b_0 = m_y - b_1*m_x
     return(b_0, b_1)
+
 def plot_regression_line(x, y, b):
 
     plt.scatter(x, y, color = "m",
@@ -32,13 +34,13 @@ def plot_regression_line(x, y, b):
     plt.ylabel('y')
 
     plt.show()
+
 def main():
-# input taken from a csv file
+#input taken from a csv file
     x_val = pd.read_csv('/Users/shubhcyanogen/Desktop/X_values.csv')
     y_val = pd.read_csv('/Users/shubhcyanogen/Desktop/Y_values.csv')
     x = x_val.values
     y = y_val.values
-
     b = estimate_coef(x, y)
     print("Estimated coefficients:\nb_0 = {} \
           \nb_1 = {}".format(b[0], b[1]))
@@ -51,6 +53,7 @@ def main():
     predictions = lm.predict(X_test)
     plt.scatter(y_test,predictions)
     print("Blue Dots Are Predictions & Red Are Given Points")
+
 # plotting regression line
     plot_regression_line(x, y, b)
 if __name__ == "__main__":
